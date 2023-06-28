5b0d488034e968a3685aa0e65240e09541c10d39
venushaamarathunga <avenusha95@gmail.com>
2023-06-28 23:46:15 +0530

    component and jsx in react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello venusha!</div>;
+  // jsx
+  const name = <h3>Venusha Amarathunga</h3>;
+  const add = <h3>Kelaniya</h3>;
+  const user = (
+    <div>
+      {name} {add}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      Hello {name}!<h1> {name} </h1>
+      I'm in {add}
+      <br />
+      {user}
+    </div>
+  );
+
+  // js function
+  const GetName = () => {
+    return "Venusha";
+  };
+
+  // react components
+  const GetNameCom = () => {
+    return <h2>New component Venusha!</h2>;
+  };
 }
 
 export default App;
