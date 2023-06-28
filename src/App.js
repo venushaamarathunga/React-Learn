d6a95ffe88410b42f7d9aac8dc84b0b7aadfcc17
venushaamarathunga <avenusha95@gmail.com>
2023-06-28 23:55:04 +0530

    components

--- src/App.js
+++ src/App.js
@@ -12,22 +12,20 @@ function App() {
 
   return (
     <div className="App">
-      Hello {name}!<h1> {name} </h1>
-      I'm in {add}
-      <br />
-      {user}
+      <UserCom />
+      <UserCom />
     </div>
   );
-
-  // js function
-  const GetName = () => {
-    return "Venusha";
-  };
-
-  // react components
-  const GetNameCom = () => {
-    return <h2>New component Venusha!</h2>;
-  };
 }
+// react components
+const UserCom = () => {
+  return (
+    <div>
+      <h2>Venusha!</h2>
+      <h2>Kelaniya!</h2>
+      <h2>27!</h2>
+    </div>
+  );
+};
 
 export default App;
